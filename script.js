38358a6261606940304d4742f509775cb3e151e6
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2020-05-05 23:32:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,60 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   lines[slideIndex-1].className += " active";
 } 
+
+//=============POPULAR ITEMS==============//
+
+productScroll();
+
+function productScroll() {
+  let slider = document.getElementById("slider");
+  let next = document.getElementsByClassName("next1");
+  let prev = document.getElementsByClassName("prev1");
+  let slide = document.getElementById("slide");
+  let item = document.getElementById("slide");
+
+  for (let i = 0; i < next.length; i++) {
+    
+
+    let position = 0; 
+
+    prev[i].addEventListener("click", function() {
+      
+      if (position > 0) {
+        position -= 1;
+        translateX(position); 
+      }
+    });
+
+    next[i].addEventListener("click", function() {
+      if (position >= 0 && position < hiddenItems()) {
+        position += 1;
+        translateX(position); 
+      }
+    });
+  }
+
+  function hiddenItems() {
+    let items = getCount(item, false);
+    let visibleItems = slider.offsetWidth / 300;
+    return items - Math.ceil(visibleItems);
+  }
+}
+
+function translateX(position) {
+  slide.style.left = position * -300 + "px";
+}
+
+function getCount(parent, getChildrensChildren) {
+  let relevantChildren = 0;
+  let children = parent.childNodes.length;
+  for (let i = 0; i < children; i++) {
+    if (parent.childNodes[i].nodeType != 3) {
+      if (getChildrensChildren)
+        relevantChildren += getCount(parent.childNodes[i], true);
+      relevantChildren++;
+    }
+  }
+  return relevantChildren;
+}
+
